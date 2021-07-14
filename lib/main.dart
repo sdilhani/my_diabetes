9eb17707e149765722e18d2228486421487c32cd
dilhani <sdilhani866@gmail.com>
2021-07-15 03:24:26 +0530

    block patern folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_diabetes/login.dart';
+import 'package:my_diabetes/src/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: LoginPage(),
-    );
-  }
+  runApp(App());
 }
